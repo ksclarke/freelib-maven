b76ad8e43d06d0c62f98f9a4cd825e9016947b65
Kevin S. Clarke <ksclarke@ksclarke.io>
2022-12-17 01:03:09 -0500

    Update TemplateInitMojo (#28)
    * Switched to using "module" names (instead of "function") and improved error checking
    * Fixed bugs
    
    Fixes #27

--- src/main/java/info/freelibrary/maven/TemplateInitMojo.java
+++ src/main/java/info/freelibrary/maven/TemplateInitMojo.java
@@ -6,17 +6,19 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.util.Objects;
+import java.nio.file.Path;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
 import info.freelibrary.util.Logger;
 import info.freelibrary.util.LoggerFactory;
+import info.freelibrary.util.StringUtils;
 
 import nu.xom.Builder;
 import nu.xom.Document;
@@ -28,7 +30,7 @@ import nu.xom.Serializer;
 /**
  * A Maven mojo that initializes a project template.
  */
-@Mojo(name = MojoNames.TEMPLATE_INIT)
+@Mojo(name = MojoNames.TEMPLATE_INIT, defaultPhase = LifecyclePhase.INITIALIZE)
 public class TemplateInitMojo extends AbstractMojo {
 
     /**
@@ -37,24 +39,49 @@ public class TemplateInitMojo extends AbstractMojo {
     static final String XMLNS = "http://maven.apache.org/POM/4.0.0";
 
     /**
+     * The POM file's artifact ID.
+     */
+    static final String ARTIFACT_ID = "artifactId";
+
+    /**
+     * The POM file's group ID.
+     */
+    static final String GROUP_ID = "groupId";
+
+    /**
+     * The POM file's version.
+     */
+    static final String VERSION = "version";
+
+    /**
      * A property name for a function's name.
      */
-    static final String FUNCTION_NAME = "function.name";
+    static final String MODULE_ARTIFACT = "module.artifact";
 
     /**
      * A property name for a function's group.
      */
-    static final String FUNCTION_GROUP = "function.group";
+    static final String MODULE_GROUP = "module.group";
 
     /**
      * A property name for the function's version.
      */
-    static final String FUNCTION_VERSION = "function.version";
+    static final String MODULE_VERSION = "module.version";
+
+    /**
+     * A property name for a function's module.
+     */
+    static final String MODULE_NAME = "module.name";
 
     /**
      * A properties element name.
      */
     static final String PROPERTIES = "properties";
+
+    /**
+     * A property to indicate if the plugin should be skipped.
+     */
+    static final String SKIP = "skip";
 
     /**
      * The logger for IfFileThenPropertiesMojo.
@@ -68,52 +95,159 @@ public class TemplateInitMojo extends AbstractMojo {
     protected MavenProject myProject;
 
     /**
-     * A path to a file to test for presence.
+     * The name of an optional module's artifactId.
      */
-    @Parameter(alias = FUNCTION_NAME, property = FUNCTION_NAME, required = true)
-    protected String myArtifactId;
+    @Parameter(alias = MODULE_ARTIFACT, property = MODULE_ARTIFACT)
+    protected String myModuleArtifact;
 
     /**
-     * A path to a file to test for absence.
+     * The name of an optional module's groupId.
      */
-    @Parameter(alias = FUNCTION_GROUP, property = FUNCTION_GROUP, required = true)
-    protected String myGroupId;
+    @Parameter(alias = MODULE_GROUP, property = MODULE_GROUP)
+    protected String myModuleGroup;
 
     /**
-     * Properties to set if the file exists.
+     * The name of an optional module's version.
      */
-    @Parameter(alias = FUNCTION_VERSION, property = FUNCTION_VERSION, defaultValue = "0.0.0-SNAPSHOT")
-    protected String myVersion;
+    @Parameter(alias = MODULE_VERSION, property = MODULE_VERSION, defaultValue = "0.0.0-SNAPSHOT")
+    protected String myModuleVersion;
+
+    /**
+     * The name of an optional module.
+     */
+    @Parameter(alias = MODULE_NAME, property = MODULE_NAME)
+    protected String myModuleName;
+
+    /**
+     * The plugin's skip flag.
+     */
+    @Parameter(alias = SKIP, property = SKIP, defaultValue = "false")
+    protected boolean myExecutionShouldBeSkipped;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        Objects.requireNonNull(myArtifactId, LOGGER.getMessage(MessageCodes.MVN_134, "artifactId"));
-        Objects.requireNonNull(myGroupId, LOGGER.getMessage(MessageCodes.MVN_134, "groupId"));
+        if (!myExecutionShouldBeSkipped) {
+            try {
+                final File pomFile = myProject.getFile();
+                final Document pom = new Builder().build(pomFile);
+                final Element root = pom.getRootElement();
+                final Element properties = getProperties(root);
+                final Elements modules = root.getChildElements("modules", XMLNS);
 
-        try {
-            final File pomFile = myProject.getFile();
-            final Document pom = new Builder().build(pomFile);
-            final Element root = pom.getRootElement();
-            final Element properties = getProperties(root);
+                // Check if we're working with a stand-alone (i.e., not a multi-module) project
+                if (modules.size() == 0) {
+                    throw new UnsupportedOperationException("Simple project templates not yet implemented");
+                }
 
-            // Check if we're working with a multi-module project
-            if (root.getChildElements("modules", XMLNS).size() != 0) {
-                throw new UnsupportedOperationException("Single project templates not yet implemented");
+                checkModuleParameters();
+                checkModuleConsistency(modules);
+
+                // Else, we have a multi-module project; in our case, used for functions
+                updateProperty(properties, MODULE_ARTIFACT, myModuleArtifact);
+                updateProperty(properties, MODULE_GROUP, myModuleGroup);
+                updateProperty(properties, MODULE_VERSION, myModuleVersion);
+
+                // Write project POM to disk
+                writePOM(pom, pomFile);
+
+                // Update the child POM too
+                updateChildPOM();
+            } catch (ParsingException | IOException details) {
+                throw new MojoExecutionException(details);
             }
+        }
+    }
 
-            // Else, we have a multi-module project; in our case, used for functions
-            updateProperty(properties, FUNCTION_NAME, myArtifactId);
-            updateProperty(properties, FUNCTION_GROUP, myGroupId);
-            updateProperty(properties, FUNCTION_VERSION, myVersion);
+    /**
+     * Write the supplied POM file to disk.
+     *
+     * @param aPOM A POM document
+     * @param aFile A POM file
+     * @throws IOException If there is trouble writing the file to disk
+     */
+    private void writePOM(final Document aPOM, final File aFile) throws IOException {
+        try (OutputStream outputStream = Files.newOutputStream(aFile.toPath())) {
+            final Serializer serializer = new Serializer(outputStream, StandardCharsets.UTF_8.name());
 
-            try (OutputStream outputStream = Files.newOutputStream(pomFile.toPath())) {
-                final Serializer serializer = new Serializer(outputStream, StandardCharsets.UTF_8.name());
+            serializer.setLineSeparator(System.lineSeparator());
+            serializer.write(aPOM);
+        }
+    }
 
-                serializer.setLineSeparator(System.lineSeparator());
-                serializer.write(pom);
+    /**
+     * Updates the child project (module), if necessary.
+     */
+    private void updateChildPOM() throws ParsingException, IOException {
+        final File pomFile = Path.of(myProject.getBasedir().toString(), myModuleName, "pom.xml").toFile();
+        final Document pom = new Builder().build(pomFile);
+        final Element root = pom.getRootElement();
+        final Element artifactId = root.getFirstChildElement(ARTIFACT_ID, XMLNS);
+        final Element groupId = root.getFirstChildElement(GROUP_ID, XMLNS);
+        final Element version = root.getFirstChildElement(VERSION, XMLNS);
+
+        if (artifactId != null) {
+            artifactId.removeChildren();
+            artifactId.appendChild(myModuleArtifact);
+        } else {
+            root.appendChild(createElement(ARTIFACT_ID, myModuleArtifact));
+        }
+
+        if (groupId != null) {
+            groupId.removeChildren();
+            groupId.appendChild(myModuleGroup);
+        } else {
+            root.appendChild(createElement(GROUP_ID, myModuleGroup));
+        }
+
+        if (version != null) {
+            version.removeChildren();
+            version.appendChild(myModuleVersion);
+        } else {
+            root.appendChild(createElement(VERSION, myModuleVersion));
+        }
+
+        writePOM(pom, pomFile);
+    }
+
+    /**
+     * Checks the parameters expected for a module template.
+     *
+     * @throws MojoExecutionException If required parameters are missing or invalid
+     */
+    private void checkModuleParameters() throws MojoExecutionException {
+        if (StringUtils.trimToNull(myModuleName) == null) {
+            throw new MojoExecutionException(LOGGER.getMessage(MessageCodes.MVN_134, MODULE_NAME));
+        }
+
+        if (StringUtils.trimToNull(myModuleGroup) == null) {
+            throw new MojoExecutionException(LOGGER.getMessage(MessageCodes.MVN_134, MODULE_GROUP));
+        }
+
+        if (StringUtils.trimToNull(myModuleArtifact) == null) {
+            throw new MojoExecutionException(LOGGER.getMessage(MessageCodes.MVN_134, MODULE_ARTIFACT));
+        }
+    }
+
+    /**
+     * Checks the module name for consistency with the defined modules.
+     *
+     * @param aModuleList A list of modules defined in the POM
+     * @throws MojoExecutionException If the expected module cannot be found
+     */
+    private void checkModuleConsistency(final Elements aModuleList) throws MojoExecutionException {
+        final Elements modules = aModuleList.get(0).getChildElements("module", XMLNS);
+        boolean hasModule = false;
+
+        for (int index = 0; index < modules.size(); index++) {
+            final Element module = modules.get(index);
+
+            if (module != null && myModuleName.equals(module.getValue())) {
+                hasModule = true;
             }
-        } catch (ParsingException | IOException details) {
-            throw new MojoExecutionException(details);
+        }
+
+        if (!hasModule) {
+            throw new MojoExecutionException(LOGGER.getMessage(MessageCodes.MVN_137, myModuleName));
         }
     }
 
@@ -125,19 +259,33 @@ public class TemplateInitMojo extends AbstractMojo {
      * @param aValue A property value
      */
     private void updateProperty(final Element aPropertyList, final String aName, final String aValue) {
-        final Element property = aPropertyList.getFirstChildElement(aName, XMLNS);
+        if (StringUtils.trimToNull(aValue) != null) {
+            Element property;
 
-        if (property != null) {
-            property.removeChildren();
-            property.appendChild(aValue);
-            LOGGER.debug(MessageCodes.MVN_132, aName, aValue);
-        } else {
-            final Element functionProperty = new Element(aName, XMLNS);
-
-            functionProperty.appendChild(aValue);
-            aPropertyList.appendChild(functionProperty);
-            LOGGER.debug(MessageCodes.MVN_133, aName, aValue);
+            if ((property = aPropertyList.getFirstChildElement(aName, XMLNS)) != null) {
+                property.removeChildren();
+                LOGGER.debug(MessageCodes.MVN_132, aName, aValue);
+                property.appendChild(aValue);
+            } else {
+                property = createElement(aName, aValue);
+                LOGGER.debug(MessageCodes.MVN_133, aName, aValue);
+                aPropertyList.appendChild(property);
+            }
         }
+    }
+
+    /**
+     * Create an XML element in our namespace using the supplied name and value.
+     *
+     * @param aName An XML element name
+     * @param aValue An element value
+     * @return A newly created XML element
+     */
+    private Element createElement(final String aName, final String aValue) {
+        final Element element = new Element(aName, XMLNS);
+
+        element.appendChild(aValue);
+        return element;
     }
 
     /**
--- src/test/java/info/freelibrary/maven/TemplateInitMojoTest.java
+++ src/test/java/info/freelibrary/maven/TemplateInitMojoTest.java
@@ -2,14 +2,21 @@
 package info.freelibrary.maven;
 
 import static info.freelibrary.maven.MojoNames.TEMPLATE_INIT;
-import static info.freelibrary.maven.TemplateInitMojo.FUNCTION_GROUP;
-import static info.freelibrary.maven.TemplateInitMojo.FUNCTION_NAME;
-import static info.freelibrary.maven.TemplateInitMojo.FUNCTION_VERSION;
+import static info.freelibrary.maven.TemplateInitMojo.ARTIFACT_ID;
+import static info.freelibrary.maven.TemplateInitMojo.GROUP_ID;
+import static info.freelibrary.maven.TemplateInitMojo.MODULE_ARTIFACT;
+import static info.freelibrary.maven.TemplateInitMojo.MODULE_GROUP;
+import static info.freelibrary.maven.TemplateInitMojo.MODULE_NAME;
+import static info.freelibrary.maven.TemplateInitMojo.MODULE_VERSION;
 import static info.freelibrary.maven.TemplateInitMojo.PROPERTIES;
+import static info.freelibrary.maven.TemplateInitMojo.SKIP;
+import static info.freelibrary.maven.TemplateInitMojo.VERSION;
 import static info.freelibrary.maven.TemplateInitMojo.XMLNS;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 import java.util.UUID;
 
@@ -18,13 +25,16 @@ import org.junit.Test;
 
 import com.google.common.io.Files;
 
+import info.freelibrary.util.FileUtils;
 import info.freelibrary.util.Logger;
 import info.freelibrary.util.LoggerFactory;
 import info.freelibrary.util.StringUtils;
 
 import nu.xom.Builder;
+import nu.xom.Document;
 import nu.xom.Element;
 import nu.xom.ParsingException;
+import nu.xom.Serializer;
 
 /**
  * Tests of the TemplateInitMojo.
@@ -45,19 +55,29 @@ public class TemplateInitMojoTest extends BetterAbstractMojoTestCase {
     private static final String TEMPLATE_POM = "target/test-classes/template-{}-pom.xml";
 
     /**
-     * A function artifactId.
+     * A module artifactId.
      */
     private static final String TEST_ARTIFACT_ID = "test-artifact";
 
     /**
-     * A function groupId.
+     * A module groupId.
      */
     private static final String TEST_GROUP_ID = "info.freelibrary";
 
     /**
-     * A function version.
+     * A module version.
      */
     private static final String TEST_VERSION = "0.0.2-SNAPSHOT";
+
+    /**
+     * A default module version.
+     */
+    private static final String DEFAULT_VERSION = "0.0.0-SNAPSHOT";
+
+    /**
+     * A function name.
+     */
+    private static final String TEST_MODULE = "test.module";
 
     /**
      * Tests the happy path for the TemplateInitMojo using the default version.
@@ -67,7 +87,8 @@ public class TemplateInitMojoTest extends BetterAbstractMojoTestCase {
     @Test
     public final void testTemplateInitMojoDefaultVersion() throws Exception {
         final File templatePOM = new File(StringUtils.format(TEMPLATE_POM, UUID.randomUUID().toString()));
-        final Properties props = getProperties(FUNCTION_NAME, TEST_ARTIFACT_ID, FUNCTION_GROUP, TEST_GROUP_ID);
+        final Properties props =
+                getProperties(MODULE_ARTIFACT, TEST_ARTIFACT_ID, MODULE_GROUP, TEST_GROUP_ID, MODULE_NAME, TEST_MODULE);
 
         Files.copy(POM, templatePOM);
 
@@ -78,9 +99,9 @@ public class TemplateInitMojoTest extends BetterAbstractMojoTestCase {
             final Element root = new Builder().build(templatePOM).getRootElement();
             final Element properties = root.getFirstChildElement(PROPERTIES, XMLNS);
 
-            assertEquals(TEST_GROUP_ID, properties.getFirstChildElement(FUNCTION_GROUP, XMLNS).getValue());
-            assertEquals(TEST_ARTIFACT_ID, properties.getFirstChildElement(FUNCTION_NAME, XMLNS).getValue());
-            assertEquals("0.0.0-SNAPSHOT", properties.getFirstChildElement(FUNCTION_VERSION, XMLNS).getValue());
+            assertEquals(TEST_GROUP_ID, properties.getFirstChildElement(MODULE_GROUP, XMLNS).getValue());
+            assertEquals(TEST_ARTIFACT_ID, properties.getFirstChildElement(MODULE_ARTIFACT, XMLNS).getValue());
+            assertEquals(DEFAULT_VERSION, properties.getFirstChildElement(MODULE_VERSION, XMLNS).getValue());
         } catch (final ParsingException | IOException details) {
             throw new MojoExecutionException(details);
         }
@@ -94,8 +115,8 @@ public class TemplateInitMojoTest extends BetterAbstractMojoTestCase {
     @Test
     public final void testTemplateInitMojoVersionSet() throws Exception {
         final File templatePOM = new File(StringUtils.format(TEMPLATE_POM, UUID.randomUUID().toString()));
-        final Properties props = getProperties(FUNCTION_NAME, TEST_ARTIFACT_ID, FUNCTION_GROUP, TEST_GROUP_ID,
-                FUNCTION_VERSION, TEST_VERSION);
+        final Properties props = getProperties(MODULE_ARTIFACT, TEST_ARTIFACT_ID, MODULE_GROUP, TEST_GROUP_ID,
+                MODULE_VERSION, TEST_VERSION, MODULE_NAME, TEST_MODULE);
 
         Files.copy(POM, templatePOM);
 
@@ -106,9 +127,47 @@ public class TemplateInitMojoTest extends BetterAbstractMojoTestCase {
             final Element root = new Builder().build(templatePOM).getRootElement();
             final Element properties = root.getFirstChildElement(PROPERTIES, XMLNS);
 
-            assertEquals(TEST_GROUP_ID, properties.getFirstChildElement(FUNCTION_GROUP, XMLNS).getValue());
-            assertEquals(TEST_ARTIFACT_ID, properties.getFirstChildElement(FUNCTION_NAME, XMLNS).getValue());
-            assertEquals(TEST_VERSION, properties.getFirstChildElement(FUNCTION_VERSION, XMLNS).getValue());
+            assertEquals(TEST_GROUP_ID, properties.getFirstChildElement(MODULE_GROUP, XMLNS).getValue());
+            assertEquals(TEST_ARTIFACT_ID, properties.getFirstChildElement(MODULE_ARTIFACT, XMLNS).getValue());
+            assertEquals(TEST_VERSION, properties.getFirstChildElement(MODULE_VERSION, XMLNS).getValue());
+        } catch (final ParsingException | IOException details) {
+            throw new MojoExecutionException(details);
+        }
+    }
+
+    /**
+     * Tests the happy path for the TemplateInitMojo with pre-existing module properties.
+     *
+     * @throws Exception If there is trouble while executing the tests.
+     */
+    @Test
+    public final void testTemplateInitMojoPreExistingProperties() throws Exception {
+        final Properties props = getProperties(MODULE_ARTIFACT, TEST_ARTIFACT_ID, MODULE_GROUP, TEST_GROUP_ID,
+                MODULE_VERSION, TEST_VERSION, MODULE_NAME, TEST_MODULE);
+        final File pomFile = addProperties(new File(StringUtils.format(TEMPLATE_POM, UUID.randomUUID().toString())));
+        final File moduleDir = new File(pomFile.getParentFile(), TEST_MODULE);
+        final Builder builder = new Builder();
+
+        // Copy our test resources
+        FileUtils.copy(new File("src/test/resources/test.module"), moduleDir);
+
+        // Run our test of the mojo
+        lookupConfiguredMojo(pomFile, props, TEMPLATE_INIT).execute();
+
+        try {
+            final Element root = builder.build(pomFile).getRootElement();
+            final Element properties = root.getFirstChildElement(PROPERTIES, XMLNS);
+            final Element module = builder.build(new File(moduleDir, "pom.xml")).getRootElement();
+
+            // Check parent POM module values
+            assertEquals(TEST_GROUP_ID, properties.getFirstChildElement(MODULE_GROUP, XMLNS).getValue());
+            assertEquals(TEST_ARTIFACT_ID, properties.getFirstChildElement(MODULE_ARTIFACT, XMLNS).getValue());
+            assertEquals(TEST_VERSION, properties.getFirstChildElement(MODULE_VERSION, XMLNS).getValue());
+
+            // Check module POM values
+            assertEquals(TEST_GROUP_ID, module.getFirstChildElement(GROUP_ID, XMLNS).getValue());
+            assertEquals(TEST_ARTIFACT_ID, module.getFirstChildElement(ARTIFACT_ID, XMLNS).getValue());
+            assertEquals(TEST_VERSION, module.getFirstChildElement(VERSION, XMLNS).getValue());
         } catch (final ParsingException | IOException details) {
             throw new MojoExecutionException(details);
         }
@@ -122,15 +181,15 @@ public class TemplateInitMojoTest extends BetterAbstractMojoTestCase {
     @Test
     public final void testTemplateInitMojoWithoutRequiredArtifactParam() throws Exception {
         final File templatePOM = new File(StringUtils.format(TEMPLATE_POM, UUID.randomUUID().toString()));
-        final Properties props = getProperties(FUNCTION_GROUP, TEST_GROUP_ID);
+        final Properties props = getProperties(MODULE_GROUP, TEST_GROUP_ID, MODULE_NAME, TEST_MODULE);
 
         Files.copy(POM, templatePOM);
 
         try {
             lookupConfiguredMojo(templatePOM, props, TEMPLATE_INIT).execute();
-            fail(LOGGER.getMessage(MessageCodes.MVN_134, "artifactId"));
-        } catch (final NullPointerException details) {
-            // This is expected
+            fail(LOGGER.getMessage(MessageCodes.MVN_134, MODULE_ARTIFACT));
+        } catch (final MojoExecutionException details) {
+            assertNotNull(details);
         }
     }
 
@@ -142,15 +201,68 @@ public class TemplateInitMojoTest extends BetterAbstractMojoTestCase {
     @Test
     public final void testTemplateInitMojoWithoutRequiredGroupParam() throws Exception {
         final File templatePOM = new File(StringUtils.format(TEMPLATE_POM, UUID.randomUUID().toString()));
-        final Properties props = getProperties(FUNCTION_NAME, TEST_ARTIFACT_ID);
+        final Properties props = getProperties(MODULE_ARTIFACT, TEST_ARTIFACT_ID, MODULE_NAME, TEST_MODULE);
 
         Files.copy(POM, templatePOM);
 
         try {
             lookupConfiguredMojo(templatePOM, props, TEMPLATE_INIT).execute();
-            fail(LOGGER.getMessage(MessageCodes.MVN_134, "groupId"));
-        } catch (final NullPointerException details) {
-            // This is expected
+            fail(LOGGER.getMessage(MessageCodes.MVN_134, MODULE_GROUP));
+        } catch (final MojoExecutionException details) {
+            assertNotNull(details);
         }
+    }
+
+    /**
+     * Tests if execution is skipped when skip flag supplied.
+     *
+     * @throws Exception If there is trouble while executing the tests.
+     */
+    @Test
+    public final void testTemplateInitMojoSkipParam() throws Exception {
+        final File templatePOM = new File(StringUtils.format(TEMPLATE_POM, UUID.randomUUID().toString()));
+        final Properties props = getProperties(SKIP, Boolean.TRUE.toString(), MODULE_NAME, TEST_MODULE);
+
+        Files.copy(POM, templatePOM);
+
+        lookupConfiguredMojo(templatePOM, props, TEMPLATE_INIT).execute();
+
+        try {
+            final Element root = new Builder().build(templatePOM).getRootElement();
+            final Element properties = root.getFirstChildElement(PROPERTIES, XMLNS);
+
+            assertNull(properties.getFirstChildElement(MODULE_VERSION, XMLNS));
+        } catch (final ParsingException | IOException details) {
+            throw new MojoExecutionException(details);
+        }
+    }
+
+    /**
+     * Adds module properties to the POM so we can test with them present too.
+     *
+     * @param aPomFile A POM file
+     * @return A POM document
+     * @throws Exception If there is trouble reading or writing the POM file.
+     */
+    private File addProperties(final File aPomFile) throws Exception {
+        final Element properties;
+        final Document pom;
+
+        Files.copy(POM, aPomFile);
+        pom = new Builder().build(aPomFile);
+
+        properties = pom.getRootElement().getFirstChildElement(PROPERTIES, XMLNS);
+        properties.appendChild(new Element(TemplateInitMojo.MODULE_NAME, XMLNS));
+        properties.appendChild(new Element(TemplateInitMojo.MODULE_ARTIFACT, XMLNS));
+        properties.appendChild(new Element(TemplateInitMojo.MODULE_GROUP, XMLNS));
+
+        try (OutputStream outputStream = java.nio.file.Files.newOutputStream(aPomFile.toPath())) {
+            final Serializer serializer = new Serializer(outputStream, StandardCharsets.UTF_8.name());
+
+            serializer.setLineSeparator(System.lineSeparator());
+            serializer.write(pom);
+        }
+
+        return aPomFile;
     }
 }
